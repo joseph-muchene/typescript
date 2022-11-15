fb8ebbae988e0d55edb1795c73f04cd4fcd11d88
joseph muchene <66246216+joseph-muchene@users.noreply.github.com>
2022-11-15 13:24:52 +0300

    refactor code

--- index.ts
+++ index.ts
@@ -14,18 +14,19 @@ let response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
 // with use of type annotations
 const todo = response.data as Todo
 
-const ID = todo.id
-const title = todo.title
-const finished = todo.completed
+logTodo(todo.id,todo.title,todo.completed)
 
-console.log(`
-The todo with ID:${ID}
-Has a title of ${title}
-Is it finished: ${finished}
 
-`)
 }
 
-const lang = "en"
-
 fetchData()
+
+const logTodo = (id:number,title:string,completed:boolean) => {
+console.log(`
+The todo with ID:${id}
+Has a title of ${title}
+Is it finished: ${completed}
+
+`)
+
+}
